dbaf00a2385ff34d750dbe896ec8ca915cb89302
dazu1990 <adrian.davilazuniga@gmail.com>
2025-02-04 13:47:06 -0500

    rough but needs direction

--- interaction_layer.js
+++ interaction_layer.js
@@ -2,7 +2,6 @@ import * as THREE from 'three';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 import { EffectComposer } from 'three/addons/postprocessing/EffectComposer.js';
 import { RenderPass } from 'three/addons/postprocessing/RenderPass.js';
-import { ShaderPass } from 'three/addons/postprocessing/ShaderPass.js';
 import { UnrealBloomPass } from 'three/addons/postprocessing/UnrealBloomPass.js';
 import { OutputPass } from 'three/addons/postprocessing/OutputPass.js';
 
