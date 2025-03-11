d255d5726d8d9159628d632cd0aa355b9699e81a
dazu1990 <adrian.davilazuniga@gmail.com>
2025-03-11 18:53:30 -0400

    SEO and FOUC fixes

--- main.js
+++ main.js
@@ -2,3 +2,6 @@ import { setupInteractionLayer } from './interaction_layer.js';
 import './style.css';
 
 setupInteractionLayer(document.querySelector('#interaction_layer_container'));
+
+// Hide the loading screen once the interaction layer is set up
+document.getElementById('loading_screen').style.display = 'none';
